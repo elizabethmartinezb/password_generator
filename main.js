f63059537a5b857fe8e9d57faa1627a3c976ab9c
Elizabeth Martinez <marieli.martinezb@gmail.com>
2024-09-16 12:55:28 -0600

    Clase 01 - Agregando la lógica de programación con javascript

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
+let numberOfCharacters = document.getElementById("quantity");
+let button = document.getElementById("generate");
 
+let password = document.getElementById('password');
+const numberMinimumOfCharacters = 8;
+const characterString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
+function generatePassword() {
+    let typedNumber = parseInt(numberOfCharacters.value);
 
+    if (typedNumber < numberMinimumOfCharacters) {
+        alert("La cantidad mínima de caracteres es " + numberMinimumOfCharacters);
+    }
 
+    let generatedPassword = '';
+    for (let index = 0; index < typedNumber; index++) {
+        
+        let randomCharacter = characterString[Math.floor(Math.random() * characterString.length)];
+        console.log(randomCharacter);
+        
+        generatedPassword += randomCharacter;
+    }
 
-
-
-
-
+    password.value = generatedPassword;
+    
+}
