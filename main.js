c0bba76728d5a9b5ce734149588d109b61e28832
Elizabeth Martinez <marieli.martinezb@gmail.com>
2024-09-17 11:47:31 -0600

    Implementando arreglo para almacenar errores de la validación de la contraseña

--- main.js
+++ main.js
@@ -39,49 +39,34 @@ function cleanFields () {
 }
 
 function validatePassword (generatedPassword) {
-
     let securePassword = 'La contraseña generada es <b>segura</b>.';
-    let insecurePassword = '';
-    
+    let errors = [];
+
     if (generatedPassword.length < numberMinimumOfCharacters) {
-        insecurePassword = 'La contraseña requiere mínimo 8 caracteres. '
-    } else {
-        counter = 1;
-        console.log(counter);
+        errors.push('La contraseña requiere mínimo 8 caracteres.');
+    } 
+
+    if (!generatedPassword.match(/[A-Z]/)) {
+        errors.push('La contraseña requiere por lo menos una letra mayúscula.');
     }
 
-    if (generatedPassword.match(/[A-Z]/)) {
-        counter += 1;
-        console.log(counter);
-    } else {
-        insecurePassword += 'La contraseña requiere por lo menos una letra mayúscula. ';
+    if (!generatedPassword.match(/[a-z]/)) {
+        errors.push('La contraseña requiere por lo menos una letra minúscula.');
     }
 
-    if (generatedPassword.match(/[a-z]/)) {
-        counter += 1;
-        console.log(counter);
-    } else {
-        insecurePassword += 'La contraseña requiere por lo menos una letra minúscula. ';
+    if (!generatedPassword.match(/\d/)) {
+        errors.push('La contraseña requiere por lo menos un número.');
     }
 
-    if (generatedPassword.match(/\d/)) {
-        counter += 1;
-        console.log(counter);
-    } else {
-        insecurePassword += 'La contraseña requiere por lo menos un número. ';
+    if (!generatedPassword.match(/[^a-zA-Z\d]/)) {
+        errors.push('La contraseña requiere por lo menos un carácter especial.');
     }
 
-    if (generatedPassword.match(/[^a-zA-Z\d]/)) {
-        counter += 1;
-        console.log(counter);
+    if (errors.length > 0) {
+        message = errors.join(' ');
     } else {
-        insecurePassword += 'La contraseña requiere por lo menos un carácter especial. ';
-    }
-
-    if (counter == 5) {
         message = securePassword;
-    } else {
-        message = insecurePassword;
     }
+
     document.getElementById('validatedPassword').innerHTML = message;
 } 
